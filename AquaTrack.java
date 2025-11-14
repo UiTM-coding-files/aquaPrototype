8227167d16330e9bf2c9d4d915a738652943a295
tontowlow <deruu0207@gmail.com>
2025-11-14 12:13:58 +0800

    change

--- AquaTrack.java
+++ AquaTrack.java
@@ -1,38 +1,18 @@
 import java.util.*;
 
-public class AquaTrack {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class AquaTrack{
+    public static void main (String[] args){
         String menu = """
-                +-----------------------+
-                |        MENU           |
-                |-----------------------|
-                | 1. Add Patient        |
-                | 2. View Records       |
-                | 3. Exit               |
-                +-----------------------+
+     ___
+    /   \\        
+   /  _  \\  
+  /  | |  \\
+ /   |_|   \\
+|   _____   |
+|  |     |  |
+|__|     |__| 
+
                 """;
-        System.out.println(menu);
-        System.out.print("Select an option: ");
-        int choice = input.nextInt();   
-        switch (choice) {
-            case 1:
-                System.out.println("Adding a new patient...");
-                // Code to add patient
-                break;
-            case 2:
-                System.out.println("Viewing records...");
-                // Code to view records
-                break;
-            case 3:
-                System.out.println("Exiting...");
-                // Code to exit
-                break;
-            default:
-                System.out.println("Invalid option. Please try again.");
-        }
-
-
+                System.out.println(menu);
     }
-
 }
