f503855cedc6a6c2c73aa34ebbffeb810776e125
tontowlow <deruu0207@gmail.com>
2025-11-14 11:52:31 +0800

    change code

--- Main.java
+++ Main.java
@@ -1,6 +1,38 @@
-public class main{
+import java.util.*;
+
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        String menu = """
+                +-----------------------+
+                |        MENU           |
+                |-----------------------|
+                | 1. Add Patient        |
+                | 2. View Records       |
+                | 3. Exit               |
+                +-----------------------+
+                """;
+        System.out.println(menu);
+        System.out.print("Select an option: ");
+        int choice = input.nextInt();   
+        switch (choice) {
+            case 1:
+                System.out.println("Adding a new patient...");
+                // Code to add patient
+                break;
+            case 2:
+                System.out.println("Viewing records...");
+                // Code to view records
+                break;
+            case 3:
+                System.out.println("Exiting...");
+                // Code to exit
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+
+
     }
-    
+
 }
